e0f2ff6a7a3cf3fca258b687452816ea28cc4956
K Ramakrishna Vaidya <kgrvaidya@gmail.com>
2020-04-19 15:41:48 +0530

    Added API and created a few components

---
+++ src/API/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        const modifiedData = {
+            confirmed: confirmed, //jUST KEEPING confirmed will also work instead of cofirmed: confirmed
+            recovered: recovered,
+            deaths: deaths,
+            lastUpdate: lastUpdate,
+        }
+
+        return modifiedData;
+    }
+    catch (error) {
+        console.log("Error : ", error);
+    }
+}
